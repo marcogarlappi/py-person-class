297b5480fab66845b090dc548d730f69fb320ecc
Marco Garlappi <marcogarlappi@italymail.biz>
2026-01-21 13:00:52 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_istances_list = []
+    for person in people:
+        one_person = Person(person["name"], person["age"])
+        if "wife" in person and person["wife"] is not None:
+            one_person.wife = person["wife"]
+        elif "husband" in person and person["husband"] is not None:
+            one_person.husband = person["husband"]
+        person_istances_list.append(one_person)
+
+    for istanced_person in person_istances_list:
+        if hasattr(istanced_person, "wife"):
+            istanced_person.wife = Person.people[istanced_person.wife]
+        elif hasattr(istanced_person, "husband"):
+            istanced_person.husband = Person.people[istanced_person.husband]
+
+    return person_istances_list
